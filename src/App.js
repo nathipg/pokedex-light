a47d396f83e66b7447aed1dd4dabeacf69f6ac6a
Nathália Pissuti <nathaliapissuti@gmail.com>
2020-03-24 13:24:19 -0300

    Backgroud

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Card from './components/Card/Card';
+
+import './App.scss';
+
+const App = () => {
+    return (
+        <div className="App">
+            TEST
+        </div>
+    );
+}
+
+export default App;
