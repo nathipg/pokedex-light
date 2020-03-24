ce9c7ccf0da6d7ced5d8412f85238856e2c2e55b
Nathália Pissuti <nathaliapissuti@gmail.com>
2020-03-24 14:08:34 -0300

    Consuming api to get pokemons

--- src/App.js
+++ src/App.js
@@ -1,13 +1,49 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 import Card from './components/Card/Card';
 
 import './App.scss';
 
 const App = () => {
+    const [pokemons, setPokemons] = useState([]);
+
+    const getPokemon = async id => {
+        try {
+            const response = await axios.get('https://pokeapi.co/api/v2/pokemon/' + id);
+
+            const pokemon = {
+                name: response.data.name,
+                number: response.data.id,
+                type: response.data.types.filter(type => type.slot === 1)[0]['type']['name']
+            };
+
+            setPokemons(prevPokemons => {
+                return [
+                    ...prevPokemons,
+                    pokemon
+                ]
+            });
+        } catch(error) {
+            console.error(error);
+        }
+    };
+
+    useEffect(() => {
+        const numbers = [...Array(150).keys()];
+        Promise.all(numbers.map(number => getPokemon(number + 1)));
+    }, []);
+
     return (
         <div className="App">
             <h1>PokeDex Light</h1>
+            {pokemons.sort((p1, p2) => p1.number - p2.number ).map(pokemon => {
+                return (
+                    <h1>
+                        {pokemon.name}
+                    </h1>
+                );
+            })}
         </div>
     );
 }
--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,9 +1,17 @@
 import React from 'react';
 
-const Card = () => {
+const Card = props => {
+    const classes = ['Card', props.type];
+    const number = props.number.padStart(3, '0');
+    
     return (
-        <div className="Card">
-
+        <div className={classes.join(' ')}>
+            <img src={`https://pokeres.bastionbot.org/images/pokemon/${props.number}.png`} alt={props.name} />
+            <div className="number">
+                <span>#{number}</span>
+            </div>
+            <h1 className="name">{props.name}</h1>
+            <span className="type">Type: {props.type}</span>
         </div>
     );
 };
